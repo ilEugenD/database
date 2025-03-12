87a8812a6e462dd7d76a994f55334914253f26c1
ilEugenD <105589224+ilEugenD@users.noreply.github.com>
2025-03-12 20:49:26 +0300

    Update index.sql

--- index.sql
+++ index.sql
@@ -1,1 +1,6 @@
-
+--
+CREATE INDEX idx_contracts_client_id ON Contracts (Client_ID);
+-- 
+CREATE INDEX idx_contracts_date_of_singing ON Contracts (Date_of_singing);
+--
+CREATE INDEX idx_addresses_address ON Addresses (Address);
